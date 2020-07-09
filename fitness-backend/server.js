67fcc831f2ebc636b2307e83311a86cf454af47b
Morgan <morgan.batterham@gmail.com>
2020-07-09 16:23:39 +1000

    schema

--- fitness-backend/server.js
+++ fitness-backend/server.js
@@ -6,6 +6,12 @@ const PORT = 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/fitness', { useNewUrlParser: true });
+const connection = mongoose.connection;
+connection.once('open', function() {
+    console.log("MongoDB database connection established successfully");
+})
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
