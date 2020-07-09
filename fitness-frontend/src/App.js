4fd8d9ae40f7d5378da53a0bcda7f662b5a638f3
Morgan <morgan.batterham@gmail.com>
2020-07-09 16:33:22 +1000

    init routes

--- fitness-frontend/src/App.js
+++ fitness-frontend/src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { render } from 'react-dom';
+import WorkOutList from './components/WorkOutList'
+import ViewWorkOut from './components/ViewWorkOut'
+import CreateWorkOut from './components/CreateWorkOut'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  return(
+    <Router>
+      <div className = "container">
+        <h2>Fitness App</h2>
+      </div>
+      <Route exact path="/" component={WorkOutList} />
+      <Route exact path="/:id" component={ViewWorkOut} />
+      <Route exact path="/create" component={CreateWorkOut} />
+    </Router>
+  )
 }
 
 export default App;
