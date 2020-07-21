2a2351769d0766276cc81fe2e14bd3b776206a49
Morgan <morgan.batterham@gmail.com>
2020-07-21 15:57:28 +1000

    JWT work

--- fitr-backend/app/controllers/users_controller.rb
+++ fitr-backend/app/controllers/users_controller.rb
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:name, :workouts)
+      params.require(:user).permit(:name, :workouts, :password, :password_confirmation)
     end
 end
--- fitr-backend/app/models/user.rb
+++ fitr-backend/app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ApplicationRecord
     has_many :workouts, dependent: :destroy
+    has_secure_password
+    validates :email, presence: true
+
+    def to_token_payload
+        {
+            sub: id,
+            email: email
+        }
+    end
 end
