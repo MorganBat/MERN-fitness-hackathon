9f7d44b9a61f6ac3aba64ebec7c2dd103b413a2b
Morgan Batterham <morgan@morganbatterham.me>
2020-07-27 10:13:51 +1000

    CORS fix

--- fitr-backend/config/application.rb
+++ fitr-backend/config/application.rb
@@ -31,11 +31,14 @@ module FitrBackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    Rails.application.config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :patch, :put]
-      end
+    config.middleware.insert_before 0, Rack::Cors do
+          allow do
+            origins '*'
+            resource '*',
+            headers: :any,
+            expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+            methods: [:get, :post, :options, :delete, :put]
+          end
     end
   end
 end
